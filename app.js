70eaef167450d1e17a5f2233aa3fbc39df06c1a5
Ania Kubow <51362610+kubowania@users.noreply.github.com>
2020-05-18 20:19:50 +0100

    Update app.js

--- app.js
+++ app.js
@@ -38,8 +38,8 @@ document.addEventListener('DOMContentLoaded', () => {
     //add numbers
     for (let i = 0; i < squares.length; i++) {
       let total = 0
-      const isLeftEdge = i % width === 0
-      const isRightEdge = i === width -1
+      const isLeftEdge = (i % width === 0)
+      const isRightEdge = (i % width === width -1)
       if (squares[i].classList.contains('valid')) {
           if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
           if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++
