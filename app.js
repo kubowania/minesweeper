d955c699b6dae0dce369c8b924f98ea9108979de
Brian <brianhoe31@gmail.com>
2021-03-24 22:47:38 -0700

    Fixed logic for adding numbers for edge cases square[0] and square[99]

--- app.js
+++ app.js
@@ -46,12 +46,12 @@ document.addEventListener('DOMContentLoaded', () => {
       if (squares[i].classList.contains('valid')) {
         if (i > 0 && !isLeftEdge && squares[i -1].classList.contains('bomb')) total ++
         if (i > 9 && !isRightEdge && squares[i +1 -width].classList.contains('bomb')) total ++
-        if (i > 10 && squares[i -width].classList.contains('bomb')) total ++
-        if (i > 11 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total ++
-        if (i < 98 && !isRightEdge && squares[i +1].classList.contains('bomb')) total ++
+        if (i > 9 && squares[i -width].classList.contains('bomb')) total ++
+        if (i > 10 && !isLeftEdge && squares[i -1 -width].classList.contains('bomb')) total ++
+        if (i < 99 && !isRightEdge && squares[i +1].classList.contains('bomb')) total ++
         if (i < 90 && !isLeftEdge && squares[i -1 +width].classList.contains('bomb')) total ++
-        if (i < 88 && !isRightEdge && squares[i +1 +width].classList.contains('bomb')) total ++
-        if (i < 89 && squares[i +width].classList.contains('bomb')) total ++
+        if (i < 89 && !isRightEdge && squares[i +1 +width].classList.contains('bomb')) total ++
+        if (i < 90 && squares[i +width].classList.contains('bomb')) total ++
         squares[i].setAttribute('data', total)
       }
     }
